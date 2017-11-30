d058defe32c53cd149eb969f36903e91148950fc
Utilisateur <user@debian.arcahe.ovh>
2017-11-30 12:32:26 +0100

    2e facon

--- compte_caractere.c
+++ compte_caractere.c
@@ -1,12 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/time.h>
+#include <string.h>
 int compt(FILE *f)
 {
 	int cpt=0;
 	while(fgetc(f)!=EOF){cpt++;}
 	return cpt;
 	}
+int compt2(FILE*f)
+{
+int cpt=0;
+char s[256];
+while (fgets(s,256,f)!=NULL)
+{cpt=cpt+strlen(s);}
+return cpt;	
+}
 int main(int argc,char**argv) {
 	FILE *f;
 	if(argc!=2)
